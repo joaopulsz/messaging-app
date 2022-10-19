47bf65698d13f7276105ec33316a817f2506d789
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-10-19 16:54:21 +0100

    added initial socket.io code

--- express.js_server/index.js
+++ express.js_server/index.js
@@ -1,7 +1,17 @@
+// SERVER SETUP
 const express = require('express');
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+// SOCKET.IO SETUP
+const socket = require("socket.io");
+
+const io = socket(server);
+
+io.on("connection", function (socket) {
+    console.log("Made socket connection");
+});
