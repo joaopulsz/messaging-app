cd0d14c9fa9cfd8428a666da20b3e7272b057e8f
April Garlington <april.garlington@hotmail.co.uk>
2022-10-20 09:49:10 +0100

    socket.io

--- express.js_server/index.js
+++ express.js_server/index.js
@@ -1,17 +1,29 @@
 // SERVER SETUP
 const express = require('express');
-
 const app = express();
+const http = require("http");
+const cors = require("cors");
+
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
-
-const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const server = http.createServer(app);
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 // SOCKET.IO SETUP
-const socket = require("socket.io");
+const {Server} = require("socket.io");
 
-const io = socket(server);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+    },
+});
 
-io.on("connection", function (socket) {
-    console.log("Made socket connection");
+io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+    })
 });
