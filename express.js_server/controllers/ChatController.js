ba18db34db8e3ee0725fa700603b562811a0b8b7
Hanqing Huang <h.hanqing@yahoo.com>
2022-10-20 16:07:08 +0100

    message

--- express.js_server/controllers/ChatController.js
+++ express.js_server/controllers/ChatController.js
@@ -1,2 +1,5 @@
 // save messages to database 
 // retrieve chats from database 
+const Chat = require('../models/Chat')
+
+//get all messages from a chat
---
+++ express.js_server/controllers/MessageController.js
@@ -0,0 +1,6 @@
+// save message to database
+// retrieve messages from database
+const Message = require('../models/Message')
+
+
+
